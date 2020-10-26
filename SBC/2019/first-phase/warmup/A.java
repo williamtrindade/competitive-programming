df7174a6912d10719fac1f368a76446b70999375
William Trindade <williamtrindade777@gmail.com>
2020-10-25 22:31:17 -0300

    SBC 2019, regionals first phase / A

---
+++ SBC/2019/first-phase/warmup/A.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class A {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int c = in.nextInt();
+        int a =  in.nextInt();
+        int travels = (a / (c - 1));
+        int rest = (a % (c - 1));
+        if (rest > 0) {
+            travels++;
+        }
+        System.out.print(travels);
+        in.close();
+    }
+}
