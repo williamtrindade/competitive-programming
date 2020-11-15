a568d795faa2c77c432f6cf41fc40c3799745427
William Trindade <williamtrindade777@gmail.com>
2020-11-15 00:47:07 -0300

    2017 ICPC regionals

--- icpc/2017/1stphase/problems/A.java
+++ icpc/2017/1stphase/problems/A.java
@@ -1,46 +1,113 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class A {
-    public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int Q = in.nextInt();
-        ArrayList<Tuple<Integer,Integer>> tuple = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    static class FastReader
+    {
+        BufferedReader br;
+        StringTokenizer st;
 
-        int vet[] = new int[N];
+        public FastReader()
+        {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
 
-        for( int i = 0; i < Q; i++ ) {
-            int A = in.nextInt();
-            int B = in.nextInt();
-
-            for ( int j = A; j <= B; j++) {
-                int value = vet[j];
-                
-                for (int i = 0; i < tuple.size(); i++) {
-                    if (tuple.get(i).getX() == value) {
-                        tuple.get(i).setY(tuple.get(i).getY()+1);
-                    }
-                    else {
-                        tuple.add(new Tuple(value, 1));
-                    }
+        String next()
+        {
+            while (st == null || !st.hasMoreElements())
+            {
+                try
+                {
+                    st = new StringTokenizer(br.readLine());
                 }
-                Tuple<Integer,Integer> bigger = new Tuple<Integer,Integer>(-1,0);
-                for (int i = 0; i < tuple.size(); i++) {
-                    if (tuple.get(i).getY() > bigger.getValue(1)) {
-                        bigger.setValue(1);
+                catch (IOException  e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        FastReader s = new FastReader();
+        // String[] numbers = s.nextInt();
+        int N = s.nextInt();
+        int Q = s.nextInt();
+
+        HashMap<Integer, Integer> t = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            t.put(i, 1);
+        }
+        HashMap<Integer, Integer> freqIndex = new HashMap<>();
+
+        for (int i = 0; i < Q; i++) {
+            int A = s.nextInt();
+            int B = s.nextInt();
+
+            // get most frequently
+            for (int f = 0; f < 9; f++) {
+                freqIndex.put(f, 0);
+            }
+            for (int j = A; j <= B; j++) {
+                int noteValue = t.get(j);
+                freqIndex.put(noteValue, freqIndex.get(noteValue)+1);
+            }
+
+            // get most appear index
+            int mostAppearIndex = 0;
+            for (int j = 0; j < 9; j++) {
+                if (freqIndex.get(j) > (int) freqIndex.get(mostAppearIndex)) mostAppearIndex = j;
+            }
+
+            // Get mostAppear index based on upper value
+            for (int j = 0; j < 9; j++) {
+                if (freqIndex.get(j).equals(freqIndex.get(mostAppearIndex))) {
+                    if (j > mostAppearIndex) {
+                        mostAppearIndex = j;
                     }
                 }
             }
-        }    
-         
-        System.out.println(N);
-
-        in.close();
+            for (int j = A; j <= B; j++) {
+                int newValueOfNote = (t.get(j) + mostAppearIndex) % 9;
+                t.put(j, newValueOfNote);
+            }
+        }
+        for (int index = 0; index < t.size(); index++) {
+            System.out.println(t.get(index));
+        }
     }
 }
