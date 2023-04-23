68e981f2eaa7c51be3def692d96101d1f50c4cd6
William Trindade <williamtrindade777@gmail.com>
2023-04-22 23:47:32 -0300

    Create main.py

---
+++ leetcode/9. Palindrome Number/main.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        rev = 0
+        orig = x
+        while x != 0:
+            rev = rev * 10 + x % 10
+            x //= 10
+        return rev == orig
+
+
+sol = Solution()
+result = sol.isPalindrome(12312)
+print(result)
