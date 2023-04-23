f055df1072537d9b12e3080e9a46a9a57bf2fcf6
William Trindade <williamtrindade777@gmail.com>
2023-04-23 16:45:05 -0300

    Create 1008.py

---
+++ beecrowd/iniciante/1008.py
@@ -0,0 +1,6 @@
+number = int(input())
+workingHours = int(input())
+valuePerHour = float(input())
+
+print('NUMBER = {:}'.format(number))
+print('SALARY = U$ {:.2f}'.format(workingHours * valuePerHour))
