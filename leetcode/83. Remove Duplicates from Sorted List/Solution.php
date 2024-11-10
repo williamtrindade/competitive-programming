47572ec1f601dce4ad052e21d0235ceaa3977d40
William Trindade <williamtrindade777@gmail.com>
2024-11-10 01:01:34 -0300

    10-11-2024

---
+++ leetcode/83. Remove Duplicates from Sorted List/Solution.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $head
+     * @return ListNode
+     */
+    function deleteDuplicates($head) {
+        $first = $head;
+        while(isset($head->next)) {
+            if ($head->val === $head->next->val) {
+                $head->next = $head->next->next;
+            }  else {
+                $head = $head->next;
+            }
+
+        }
+        return $first;
+    }
+}
+
+
+/**
+ * Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,1,2]
+ * Output: [1,2]
+ * Example 2:
+ *
+ *
+ * Input: head = [1,1,2,3,3]
+ * Output: [1,2,3]
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the list is in the range [0, 300].
+ * -100 <= Node.val <= 100
+ * The list is guaranteed to be sorted in ascending order.
+ *
+ */
